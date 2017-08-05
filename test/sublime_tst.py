af3d7a3124eea1673bf68eeaead6956333bd7b31
Bingyu Xia <xiaby14@mails.tsinghua.edu.cn>
2017-08-05 14:55:01 +0800

    initial

---
+++ test/sublime_tst.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+
+def print_number_01():
+	for i in np.linspace(1, 100, 10):
+		print(i)
+def print_number_02():
+	for value in np.linspace(1, 100, 10):
+		print(value)
+def print_number_03():
+	for j in np.linspace(1, 100, 10):
+		print(j)
+
+print_number_03()
+
